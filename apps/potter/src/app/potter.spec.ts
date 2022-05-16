42f5e7669433a777cb8cae69175db05c89a5e466
Huskyee <vincent880531@gmail.com>
2022-05-16 10:37:35 +0800

    refactor

--- apps/potter/src/app/potter.spec.ts
+++ apps/potter/src/app/potter.spec.ts
@@ -1,24 +1,27 @@
 import { Potter } from './potter';
 
 describe('Potter', () => {
+  let potter: Potter;
+
+  beforeEach(() => {
+    potter = new Potter();
+  });
+
   it('should create an instance', () => {
-    expect(new Potter()).toBeTruthy();
+    expect(potter).toBeTruthy();
   });
 
   test('buy nothing', () => {
-    const potter = new Potter();
     potter.buy([]);
     expect(potter.price).toBe(0);
   });
 
   test('buy one book', () => {
-    const potter = new Potter();
     potter.buy([0]);
     expect(potter.price).toBe(8);
   });
 
   test('buy same books', () => {
-    const potter = new Potter();
     potter.buy([0, 0, 0]);
     expect(potter.price).toBe(24);
   });
