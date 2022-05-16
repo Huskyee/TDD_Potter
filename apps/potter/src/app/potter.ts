813bcfa4eb1ae211459216bf6f8129d9959e40c9
Huskyee <vincent880531@gmail.com>
2022-05-16 10:47:29 +0800

    test4: buy two different books -> green light

--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -1,9 +1,20 @@
 export class Potter {
     private _total = 0;
     private _unitPrice = 8;
+    private _books = [0, 0, 0, 0, 0];
+    private _numberOfTypes = 0;
     buy(booksIndices: number[]) {
         for(let i=0; i<booksIndices.length; i++) {
+            this._books[booksIndices[i]] += 1;
             this._total += this._unitPrice;
+        }
+        for(let i=0; i<this._books.length; i++) {
+            if(this._books[i]) {
+                this._numberOfTypes++;
+            }
+        }
+        if(this._numberOfTypes == 2) {
+            this._total *= 0.95;
         }
     }
 
